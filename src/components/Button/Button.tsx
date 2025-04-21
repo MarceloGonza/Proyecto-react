9536778075d99d97e3bf4a83873d05fec8e48db0
Marcelo Gonzalez <marceloalbertogonzalez3@gmail.com>
2025-04-21 11:18:49 -0300

    Proyecto inicializado

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,15 @@
+// Button.tsx
+import "./Button.css";
+
+interface Props {
+  label: string;
+  parentMethod: () => void;
+}
+
+export const Button = ({ label, parentMethod }: Props) => {
+  return (
+    <button className="custom-button" onClick={parentMethod}>
+      {label}
+    </button>
+  );
+};
