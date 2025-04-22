670a2889745b153b0561ba309870740d0cdb8e75
Marcelo Gonzalez <marceloalbertogonzalez3@gmail.com>
2025-04-22 10:48:37 -0300

    use state

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,21 @@ import { Button } from "./components/Button/Button";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("Marcelo");
 
   const countMore = () => {
     setCount((count) => count + 1);
   };
 
+  const changeName = () => {
+    setName("Gonzalez");
+  };
+
   return (
     <>
       <Button label={`Count is ${count}`} parentMethod={countMore} />
+      <p>{name}</p>
+      <Button label="changeName" parentMethod={changeName} />
     </>
   );
 }
